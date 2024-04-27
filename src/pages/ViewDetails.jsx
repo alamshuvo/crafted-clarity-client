5d01c8f0891e79fa304c9d68f45097eabef0b60b
iftakhar <iftakharalamshuvo11@gmail.com>
2024-04-27 22:19:55 +0600

    view details page design

--- src/pages/ViewDetails.jsx
+++ src/pages/ViewDetails.jsx
@@ -1,4 +1,4 @@
-import { useLoaderData } from "react-router-dom";
+import { Link, useLoaderData } from "react-router-dom";
 import { Typewriter } from "react-simple-typewriter";
 
 const ViewDetails = () => {
@@ -34,10 +34,9 @@ const ViewDetails = () => {
                   `${data.selectCategory}`,
                   `${data.price}`,
                   `${data.stockStatus}`,
-                  `${data.description.slice(1,40)}`,
+                  `${data.description.slice(1, 40)}`,
                   `${data.name}`,
                   `${data.email}`,
-                  
                 ]}
                 loop={5}
                 cursor
@@ -50,26 +49,63 @@ const ViewDetails = () => {
               />
             </span>
           </h1>
-          <p className="mt-6 mb-8 text-lg sm:mb-12">
-            Dictum aliquam porta in condimentum ac integer
-            <br className="hidden md:inline lg:hidden" />
-            turpis pulvinar, est scelerisque ligula sem
-          </p>
-          <div className="flex flex-col space-y-4 sm:items-center sm:justify-center sm:flex-row sm:space-y-0 sm:space-x-4 lg:justify-start">
-            <a
-              rel="noopener noreferrer"
-              href="#"
-              className="px-8 py-3 text-lg font-semibold rounded bg-violet-400 dark:bg-violet-600 text-gray-900 dark:text-gray-50"
-            >
-              Suspendisse
-            </a>
-            <a
-              rel="noopener noreferrer"
-              href="#"
-              className="px-8 py-3 text-lg font-semibold border rounded border-gray-100 dark:border-gray-800"
-            >
-              Malesuada
-            </a>
+          <h1>{data.description}</h1>
+          <div className="mt-5">
+            <h1 className="text-2xl">Price: {data.price}</h1>
+            <h1 className="text-2xl">customization: {data.customization}</h1>
+            <div className="rating mt-5 flex justify-between">
+              <h1>Rating :</h1>
+              <h1>{data.rating}</h1>
+            <div>
+            <input
+                type="radio"
+                name="rating-2"
+                className="mask mask-star-2 bg-[#E5DDC5]"
+                checked
+              />
+              <input
+                type="radio"
+                name="rating-2"
+                className="mask mask-star-2 bg-[#E5DDC5]"
+              />
+              <input
+                type="radio"
+                name="rating-2"
+                className="mask mask-star-2 bg-[#E5DDC5]"
+              />
+              <input
+                type="radio"
+                name="rating-2"
+                className="mask mask-star-2 bg-[#E5DDC5]"
+              />
+              <input
+                type="radio"
+                name="rating-2"
+                className="mask mask-star-2 bg-[#E5DDC5]"
+                
+              />
+            </div>
+               
+               <div>
+                {data.stockStatus}
+               </div>
+            </div>
+            <div>
+              <p>Processing Time :{data.processing_time} Weeks</p>
+            </div>
+           
+          </div>
+
+          <div className="flex flex-col mt-5 space-y-4 sm:items-center sm:justify-center sm:flex-row sm:space-y-0 sm:space-x-4 lg:justify-start">
+            <Link to={"/"}>
+              <button
+                rel="noopener noreferrer"
+                href="#"
+                className="px-8 py-3 text-lg font-semibold rounded bg-[#F1EEDC] dark:bg-[#F1EEDC] text-gray-900 dark:text-gray-50"
+              >
+                Go Home
+              </button>
+            </Link>
           </div>
         </div>
         <div className="flex items-center justify-center p-6 mt-8 lg:mt-0 h-72 sm:h-80 lg:h-96 xl:h-112 2xl:h-128">
